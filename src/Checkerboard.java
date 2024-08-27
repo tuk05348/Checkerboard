585009a296c8ebc6c8660916f57e677093458232
Niaz Baharudeen <tuk05348@temple.edu>
2024-08-27 18:47:51 -0400

    Wrote a header comment with the program name, author, date and a short description of what the program will do.

--- src/Checkerboard.java
+++ src/Checkerboard.java
@@ -1,8 +1,15 @@
-
+/*
+ * Checkerboard
+ * Niaz Baharudeen
+ * 08/27/2024
+ * Takes in an integer as input and prints to screen a checkerboard of
+ * asterisks and spaces using the integer as the number of asterisks.
+ * Each row is offset from the previous row.
+ */
 public class Checkerboard {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
